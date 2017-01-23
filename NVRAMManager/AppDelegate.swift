a54d2c7835718083f5ed0af3c3434534e775f280
Gary Niu <gary.niu@gmail.com>
2017-01-23 16:00:38 +0800

    Initial Commit

---
+++ NVRAMManager/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NVRAMManager
+//
+//  Created by 牛秀元 on 2017/1/23.
+//  Copyright © 2017年 牛秀元. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
